eb2a2fc9b9f07d1cf4b7fd79bebb59a2f0484d20
rinnifox <karina.m.pats@gmail.com>
2018-09-18 10:05:42 +0300

    added task1

--- task1/task1.py
+++ task1/task1.py
@@ -1,3 +1,5 @@
+import time
+
 #  This function should return sorted list of distinct elements of e
 def unique(e):
     return sorted(set(e))
@@ -10,7 +12,7 @@ def transposeDict(d):
 
 #  This function should return minimal positive integer which is not present at list e
 def mex(e):
-    return 1 if 1 not in e else min([x+1 for x in [elem for elem in e if type(elem) == int] if x+1 not in e])
+    return min([x for x in range(1, len(e)+2) if x not in e])
 
 
 #  This function should return dict with counts of every symbol from string
